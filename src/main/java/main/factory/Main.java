7c8190901474f87977c7e24855fe950b2249cd32
Milomir Dragović <milomir.dragovic@cubes.rs>
2020-02-11 20:17:24 +0100

    Refactors Car class

--- src/main/java/main/factory/Main.java
+++ src/main/java/main/factory/Main.java
@@ -11,31 +11,14 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Car fiat = new Car("Fiat Punto", 2010, "black", 45);
+        fiat.showData();
         
+        Car ford = new Car("Ford", 2015, "red", 40, 10, 5);
+        ford.showData();
         
-        System.out.println("Pocetak");
-        
-        String message = "Ovo je poruka";
-        
-        Car fiat = new Car();
-        fiat.model = "Fiat Punto";
-        fiat.buildYear = 2010;
-        fiat.color = "black";
-        fiat.consumption = 6;
-        fiat.maxSpeed = 180;
-        fiat.currentFuel = 20;
-        fiat.maxFuel = 45;
-        
-        
-        System.out.println("Model: " + fiat.model);
-        System.out.println("Godina proizvodnje: " + fiat.buildYear);
-        
-        Car ford = new Car();
-        ford.model = "Ford Fiesta";
-        ford.buildYear = 2015;
-        
-        System.out.println("Model: " + ford.model);
-        System.out.println("Godina proizvodnje: " + ford.buildYear);
+        Car lada = new Car("Lada Niva", 1980, "green", 50, 40, 12);
+        lada.showData();
         
     }
     
